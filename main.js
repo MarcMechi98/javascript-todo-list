8227a9f7b14ccb1232a922b099ebea4fb98250f5
Marcelo Mechi <marcedumechi@gmail.com>
2022-11-23 21:22:10 -0300

    Added js

--- main.js
+++ main.js
@@ -1,3 +1,110 @@
-// window.addEventListener('load', () => {
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTodoForm = document.querySelector('#new-todo-form');
 
-// })
+    const username = localStorage.getItem('username') || '';
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+
+    });
+
+    // Getting Json Code from the database
+    newTodoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+
+        displayTodos();
+    });
+});
+
+//Making the todo list show up
+function displayTodos() {
+    const todoList = document.querySelector('#todo-list');
+
+    todoList.innerHTML = '';
+
+    todos.forEach(todo => {
+
+        //Creating the elements
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const editButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        //Checking if it's already done
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('bubble');
+
+        //Checking the todo category
+        if (todo.category === 'personal') {
+            span.classList.add('personal');
+        }
+        span.classList.add('business');
+
+        //Todo content
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        editButton.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${todo.content}"
+         readonly>`;
+        editButton.innerHTML = 'Edit';
+        deleteButton.innerHTML = 'Delete';
+
+        //Appending the new elements
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(editButton);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+        todoList.appendChild(todoItem);
+
+        //Crossing out completed todos
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener('click', e => {
+            todo.done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+            if (todo.done) {
+                todoItem.classList.add('done');
+            }
+            todoItem.classList.remove('done');
+
+            displayTodos();
+        })
+
+        //Making the "edit" button work
+
+
+
+
+
+
+    });
+}
